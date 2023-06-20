035c8a194b42496f5b1e6616cdad0320a236b3f2
hogeno <masshi1108@gmail.com>
2023-06-20 22:57:56 +0900

    [update] check keyEvent regularly

--- pound/src/main.rs
+++ pound/src/main.rs
@@ -1,7 +1,37 @@
-use std::io;
-use std::io::Read;
+use crossterm::event::{Event, KeyCode, KeyEvent};
+use crossterm::{event, terminal};
+use std::time::Duration;
+
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Failed to disable raw mode");
+    }
+}
 
 fn main() {
-    let mut buf = [0; 1];
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
+    let _cleanup = CleanUp;
+    terminal::enable_raw_mode().expect("Failed to enable raw mode");
+
+    loop {
+        if event::poll(Duration::from_millis(500)).expect("Error") {
+            if let Event::Key(event) = event::read().expect("Failed to read line") {
+                match event {
+                    KeyEvent {
+                        code: KeyCode::Char('q'),
+                        modifiers: event::KeyModifiers::NONE,
+                        kind: event::KeyEventKind::Press,
+                        state: event::KeyEventState::NONE,
+                    } => break,
+                    _ => {
+                        //todo
+                    }
+                }
+                println!("{:?}\r", event);
+            };
+        } else {
+            println!("No input yet\r");
+        }
+    }
 }
